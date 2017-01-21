3d65e6500bbf6bb04bcce0083d131a9d02c02d03
John Harris <johnnytronic@gmail.com>
2017-01-21 10:50:28 -0500

    + Basic folders + Event system

---
+++ Assets/scripts/Events.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace BeamMeRoundScotty{
+
+/*
+    public class SceneChangeRequestEvent : GameEvent
+    {
+        
+        //public Scene fromScene;
+        public string toSceneName;
+
+        public SceneChangeRequestEvent(string toSceneName)
+        {
+          //  this.fromScene = fromScene;
+            this.toSceneName = toSceneName;
+        }
+    }
+*/
+  
+}
