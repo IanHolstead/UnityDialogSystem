b502d26e3414f7169309fce72c294185af780fde
Ian Holstead <ian.holstead@gmail.com>
2017-01-21 20:42:13 -0500

    Choices are working!

--- Assets/scripts/CutsceneManager.cs
+++ Assets/scripts/CutsceneManager.cs
@@ -6,35 +6,37 @@ using UnityEngine.UI;
 namespace BeamMeRoundScotty{
 	public class CutsceneManager : MonoBehaviour {
 
-		Canvas screenCanvas;
+        private enum WritingState
+        {
+            Hidden,
+            WritingLine,
+            WritingChoice,
+            WaitingForChoice,
+            FinishedLine,
+        }
 
-		public GameObject dialogPanelRef;
-		public Text speakerNameTextRef;
-		public Text spokenTextRef;
+        private WritingState writingState = WritingState.Hidden;
 
-		public float charactersPerSecond = 3f;
-
-		private enum WritingState{
-			Hidden,
-			WritingLine,
-			WritingChoice,
-			WaitingForChoice,
-			FinishedLine,
-		}
-		private WritingState writingState = WritingState.Hidden;
+        int currentLineIndex;
+		string currentLineText = "";
+		int currentLineLength;
+		float currentLinePosition;
+        public float charactersPerSecond = 3f;
 
         LineSet currentLineSet;
         TempLineSet currentTempLineSet;
 
-		int currentLineIndex;
-		string currentLineText = "";
-		int currentLineLength;
-		float currentLinePosition;
-
-		TempChoiceSet currentTempChoiceSet;
-
+        TempChoiceSet currentTempChoiceSet;
         ChoiceSet currentChoiceSet;
 
+
+        [Header("Canvas References")]
+        Canvas screenCanvas;
+
+        public GameObject dialogPanelRef;
+        public Text speakerNameTextRef;
+        public Text spokenTextRef;
+        
         public GameObject choicePanelRef;
 
 		public GameObject choicePanel0Ref;
@@ -48,8 +50,9 @@ namespace BeamMeRoundScotty{
 		public Text choice3TextRef;
 
         public ActionBaseClass LineSetForQKey;
+        public ActionBaseClass LineSetForWKey;
 
-		public class ActionSet{
+        public class ActionSet{
 			
 		}
 
@@ -79,10 +82,10 @@ namespace BeamMeRoundScotty{
 			screenCanvas = GameObject.FindGameObjectWithTag("ScreenCanvas").GetComponent<Canvas>();
 
             Events.instance.AddListener<CutsceneEvent>(OnCutsceneEvent);
-			Events.instance.AddListener<TempCutsceneEvent>(OnTempCutsceneEvent);
 
 			if(writingState == WritingState.Hidden){
-				dialogPanelRef.SetActive(false);
+                HideChoicePanel();
+                dialogPanelRef.SetActive(false);
 			}
 		}
 		
@@ -94,136 +97,111 @@ namespace BeamMeRoundScotty{
                 Events.instance.Raise(new CutsceneEvent(LineSetForQKey));
             }
 
-			if(Input.GetKeyDown(KeyCode.L)){
-
-				string[] testLines = {"Bacon ipsum dolor amet tenderloin short ribs ribeye id eiusmod. Ham hock shoulder flank laborum porchetta velit hamburger cupim pork loin rump ut ullamco cupidatat ut pork belly. Cupim ea dolore meatball ground round shank id turkey ut drumstick eiusmod. Consectetur irure deserunt swine ground round fatback.", "Hello?", "Is anybody there?"};
-				TempLineSet exampleLineSet = new TempLineSet(testLines);
-
-				Events.instance.Raise(new TempCutsceneEvent(exampleLineSet));
-			}
-
-			if(Input.GetKeyDown(KeyCode.C)){
-
-				string[] convo0 = new string[]{"It was then that I saw it...", "A mutant cow!"};
-				TempLineSet lineSet0 = new TempLineSet(convo0);
-
-				string[] convo1 = new string[]{"Whatever you say.", "That coffee mug was getting pretty old anyway."};
-				TempLineSet lineSet1 = new TempLineSet(convo1);
-
-				string[] convo2 = new string[]{"You put your right hand in.", "You take your right hand out.", "You put your right hand in and you shake it all about."};
-				TempLineSet lineSet2 = new TempLineSet(convo2);
-
-				string[] convo3 = new string[]{"There are 10 kinds of people in this world.", "Those who understand binary and those who don.t"};
-				TempLineSet lineSet3 = new TempLineSet(convo3);
-
-				string[] choices = {"It was a dark and stormy night...", "I suppose you're right...", "Let's dance!", "Excelsior!"};
-				TempChoiceSet choiceSet = new TempChoiceSet("As I see it, you've got a few choices...", choices, new ActionSet[]{lineSet0, lineSet1, lineSet2, lineSet3});
-
-				Events.instance.Raise(new TempCutsceneEvent(choiceSet));
-			}
-
-			switch(writingState){
-			case WritingState.WritingLine:
-				{
-					//Advance to the end of the line if the player presses space
-					if(Input.GetKeyDown(KeyCode.Space)){
-						FinishWriting();
-					}
-
-					//Advance the writing "cursor" over time
-					if(currentLinePosition < currentLineLength){
-						//Advance our cursor position
-						currentLinePosition = Mathf.Clamp(currentLinePosition + Time.deltaTime * charactersPerSecond, 0f, currentLineLength);
-					}
-
-					//Update the displayed Text
-					spokenTextRef.text = currentLineText.Substring(0, (int)currentLinePosition);
-
-					if(currentLinePosition == currentLineText.Length){
-						writingState = WritingState.FinishedLine;
-					}	
-					break;	
-				}
-
-			case WritingState.FinishedLine:
-				{
-					if(Input.GetKeyDown(KeyCode.Space)){
-						AdvanceToNextLine();
-					}
-					break;
-				}
-
-			case WritingState.WritingChoice:
-				{
-					//Advance to the end of the line if the player presses space
-					if(Input.GetKeyDown(KeyCode.Space)){
-						FinishWriting();
-					}
-
-					//Advance the writing "cursor" over time
-					if(currentLinePosition < currentLineLength){
-						//Advance our cursor position
-						currentLinePosition = Mathf.Clamp(currentLinePosition + Time.deltaTime * charactersPerSecond, 0f, currentLineLength);
-					}
-
-					//Update the displayed Text
-					spokenTextRef.text = currentLineText.Substring(0, (int)currentLinePosition);
-
-					if(currentLinePosition == currentLineText.Length){
-						writingState = WritingState.WaitingForChoice;
-					}	
-
-					//Listen for choice
-					ListenForChoice();
-
-					break;
-				}
-
-			case WritingState.WaitingForChoice:
-				{
-					ListenForChoice();
-					break;	
-				}
-
-
-			}
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                Events.instance.Raise(new CutsceneEvent(LineSetForWKey));
+            }
+            PlayText();
 		}
+
+        void PlayText()
+        {
+            switch (writingState)
+            {
+                case WritingState.WritingLine:
+                    {
+                        //Advance to the end of the line if the player presses space
+                        if (Input.GetKeyDown(KeyCode.Space))
+                        {
+                            FinishWriting();
+                        }
+
+                        //Advance the writing "cursor" over time
+                        if (currentLinePosition < currentLineLength)
+                        {
+                            //Advance our cursor position
+                            currentLinePosition = Mathf.Clamp(currentLinePosition + Time.deltaTime * charactersPerSecond, 0f, currentLineLength);
+                        }
+
+                        //Update the displayed Text
+                        spokenTextRef.text = currentLineText.Substring(0, (int)currentLinePosition);
+
+                        if (currentLinePosition == currentLineText.Length)
+                        {
+                            writingState = WritingState.FinishedLine;
+                        }
+                        break;
+                    }
+
+                case WritingState.FinishedLine:
+                    {
+                        if (Input.GetKeyDown(KeyCode.Space))
+                        {
+                            AdvanceToNextLine();
+                        }
+                        break;
+                    }
+
+                case WritingState.WritingChoice:
+                    {
+                        Debug.Log("Writing Choice");
+                        //Advance to the end of the line if the player presses space
+                        if (Input.GetKeyDown(KeyCode.Space))
+                        {
+                            FinishWriting();
+                        }
+
+                        //Advance the writing "cursor" over time
+                        if (currentLinePosition < currentLineLength)
+                        {
+                            //Advance our cursor position
+                            currentLinePosition = Mathf.Clamp(currentLinePosition + Time.deltaTime * charactersPerSecond, 0f, currentLineLength);
+                        }
+
+                        //Update the displayed Text
+                        spokenTextRef.text = currentLineText.Substring(0, (int)currentLinePosition);
+
+                        if (currentLinePosition == currentLineText.Length)
+                        {
+                            writingState = WritingState.WaitingForChoice;
+                        }
+
+                        //Listen for choice
+                        ListenForChoice();
+
+                        break;
+                    }
+
+                case WritingState.WaitingForChoice:
+                    {
+                        Debug.Log("Writing Choice");
+                        ListenForChoice();
+                        break;
+                    }
+            }
+        }
 
         void OnCutsceneEvent(CutsceneEvent e)
         {
             PlayActionSet(e.actionSet);
         }
 
-        void OnTempCutsceneEvent(TempCutsceneEvent e){
-
-			PlayTempActionSet(e.actionSet);
-		}
-
         void PlayActionSet(ActionBaseClass actionSet)
         {
             Debug.Log(actionSet.GetType().ToString());
             if (actionSet is LineSet)
             {
-                print("Did I make it here?");
                 PlayLineSet((LineSet)actionSet);
             }
             else if (actionSet is ChoiceSet)
             {
                 PlayChoiceSet((ChoiceSet)actionSet);
+                Debug.Log("choice");
             }
         }
 
-        void PlayTempActionSet(ActionSet actionSet){
-			if(actionSet is TempLineSet){
-				PlayTempLineSet((TempLineSet)actionSet);
-			} else if(actionSet is TempChoiceSet){
-				PlayTempChoiceSet((TempChoiceSet)actionSet);
-			}
-		}
-
         void PlayLineSet(LineSet lineSet)
         {
-
             writingState = WritingState.WritingLine;
 
             currentChoiceSet = null;
@@ -235,48 +213,17 @@ namespace BeamMeRoundScotty{
             BeginWritingLine(currentLine.line, currentLine.speakerName);
 
             ShowPanel();
-
             HideChoicePanel();
         }
 
-        void PlayTempLineSet(TempLineSet lineSet){
-
-			writingState = WritingState.WritingLine;
-
-			currentTempChoiceSet = null;
-			currentTempLineSet = lineSet;
-			currentLineIndex = 0;
-
-			BeginWritingLine(currentTempLineSet.lines[currentLineIndex]);
-
-			ShowPanel();
-
-			HideChoicePanel();
-		}
-
-		void PlayTempChoiceSet(TempChoiceSet choiceSet){
-
-			writingState = WritingState.WritingChoice;
-
-			currentTempLineSet = null;
-			currentTempChoiceSet = choiceSet;
-
-			BeginWritingLine(choiceSet.mainText);
-			PopulateTempChoices(choiceSet.choices);
-
-			ShowPanel();
-			ShowChoicePanel();
-		}
-
-
         void PlayChoiceSet(ChoiceSet choiceSet)
         {
-
             writingState = WritingState.WritingChoice;
 
             currentLineSet = null;
             currentChoiceSet = choiceSet;
 
+            Debug.Log(choiceSet.lineToPlay.line);
             BeginWritingLine(choiceSet.lineToPlay.line, choiceSet.lineToPlay.speakerName);
             PopulateChoices(choiceSet.choices);
 
@@ -309,41 +256,31 @@ namespace BeamMeRoundScotty{
             currentLinePosition = 0;
         }
 
-        [System.Obsolete]
-		void BeginWritingLine(string line){
-			
-			currentLineText = line;
-			currentLineLength = line.Length;
-			currentLinePosition = 0;
-		}
-
 		void FinishWriting(){
 			currentLinePosition = currentLineLength;	
 		}
 
-		void AdvanceToNextLine(){
-			
-			//Are there more lines in this set?
-			if(currentTempLineSet != null && currentLineIndex + 1 < currentTempLineSet.lines.Length){
-				currentLineIndex++;
-
-				BeginWritingLine(currentTempLineSet.lines[currentLineIndex]);
-				writingState = WritingState.WritingLine;
-
-            }
-            else if (currentLineSet != null && currentLineIndex + 1 < currentLineSet.lines.Count)
+        void AdvanceToNextLine()
+        {
+            if (currentLineSet != null && currentLineIndex + 1 < currentLineSet.lines.Count)
             {
                 currentLineIndex++;
 
                 BeginWritingLine(currentLineSet.lines[currentLineIndex].line, currentLineSet.lines[currentLineIndex].speakerName);
                 writingState = WritingState.WritingLine;
             }
-            else {
-				//We're down with our current set of lines, close the dialog panel
-                //TODO: we need to check whats next!
-				HidePanel();
-			}
-		}
+            else
+            {
+                if (currentLineSet.doNext != null)
+                {
+                    PlayActionSet(currentLineSet.doNext);
+                }
+                else
+                {
+                    HidePanel();
+                }
+            }
+        }
 
         void PopulateChoices(List<Choice> choices)
         {
@@ -375,64 +312,38 @@ namespace BeamMeRoundScotty{
                 choicePanel3Ref.SetActive(true);
                 choice3TextRef.text = choices[3].shortText;
             }
-
         }
 
-        void PopulateTempChoices(string[] choicesText){
-			choicePanel0Ref.SetActive(false);
-			choicePanel1Ref.SetActive(false);
-			choicePanel2Ref.SetActive(false);
-			choicePanel3Ref.SetActive(false);
-
-			if(choicesText.Length > 0){
-				choicePanel0Ref.SetActive(true);
-				choice0TextRef.text = choicesText[0];
-			}
-
-			if(choicesText.Length > 1){
-				choicePanel1Ref.SetActive(true);
-				choice1TextRef.text = choicesText[1];
-			}
-
-			if(choicesText.Length > 2){
-				choicePanel2Ref.SetActive(true);
-				choice2TextRef.text = choicesText[2];
-			}
-
-			if(choicesText.Length > 3){
-				choicePanel3Ref.SetActive(true);
-				choice3TextRef.text = choicesText[3];
-			}
-				
-		}
-
-		void ListenForChoice(){
-			if(Input.GetKeyDown(KeyCode.A)){
-
-				if(currentTempChoiceSet.choices.Length > 0){
-					PlayTempActionSet(currentTempChoiceSet.nextActions[0]);
-				}
-				
-			} else if(Input.GetKeyDown(KeyCode.D)){
-
-				if(currentTempChoiceSet.choices.Length > 1){
-					PlayTempActionSet(currentTempChoiceSet.nextActions[1]);
-				}
-
-			} else if(Input.GetKeyDown(KeyCode.S)){
-
-				if(currentTempChoiceSet.choices.Length > 2){
-					PlayTempActionSet(currentTempChoiceSet.nextActions[2]);
-				}
-
-			} else if(Input.GetKeyDown(KeyCode.W)){
-
-				if(currentTempChoiceSet.choices.Length > 3){
-					PlayTempActionSet(currentTempChoiceSet.nextActions[3]);
-				}
-			}
-
-
-		}
+        void ListenForChoice()
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                if (currentChoiceSet.choices.Count > 0)
+                {
+                    PlayActionSet(currentChoiceSet.choices[0].doNext);
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                if (currentChoiceSet.choices.Count > 1)
+                {
+                    PlayActionSet(currentChoiceSet.choices[1].doNext);
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                if (currentChoiceSet.choices.Count > 2)
+                {
+                    PlayActionSet(currentChoiceSet.choices[2].doNext);
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.W))
+            {
+                if (currentChoiceSet.choices.Count > 3)
+                {
+                    PlayActionSet(currentChoiceSet.choices[3].doNext);
+                }
+            }
+        }
 	}
 }
