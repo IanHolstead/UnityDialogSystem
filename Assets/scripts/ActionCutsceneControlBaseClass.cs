11f561831ab6d8b3a750a5cd464aa46727275198
Ian Holstead <ian.holstead@gmail.com>
2017-01-22 11:02:48 -0500

    added action cutscene base class

---
+++ Assets/scripts/ActionCutsceneControlBaseClass.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public abstract class ActionCutsceneControlBaseClass : MonoBehaviour {
+
+    Camera cameraRef;
+    Camera oldCamera;
+
+    protected void Start()
+    {
+        cameraRef = GetComponent<Camera>();
+        gameObject.SetActive(false);
+    }
+
+    public void Play()
+    {
+        SetupCutscene();
+    }
+
+    public void Finish()
+    {
+        CleanupCutscene();
+    }
+
+    protected void SetupCutscene()
+    {
+        //pause game
+        gameObject.SetActive(true);
+        oldCamera = Camera.current;
+        oldCamera.enabled = false;
+    }
+
+    protected void CleanupCutscene()
+    {
+        oldCamera.enabled = true;
+        oldCamera = null;
+        gameObject.SetActive(false);
+    }
+}
